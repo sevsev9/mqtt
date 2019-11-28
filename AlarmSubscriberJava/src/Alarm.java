fde6019b5af689e5057818e4acc9cedab7719b26
NicoOhler <32089075+NicoOhler@users.noreply.github.com>
2019-11-28 11:48:56 +0100

    Ich habe all meine Passwörter in "warnichtkorrekt" geändert, so sagt mir mein Rechner wie es lautet wenn ich es vergessen habe.

--- AlarmSubscriberJava/src/Alarm.java
+++ AlarmSubscriberJava/src/Alarm.java
@@ -1,16 +1,16 @@
-import java.sql.Date;
+import java.sql.Timestamp;
 
 /**
  * Created by Nico Ohler on 21.11.2019.
  */
 public class Alarm {
     private int id;
-    private Date timestamp;
+    private Timestamp timestamp;
     private String description;
     private String handler;
     private String origin;
 
-    public Alarm(int id, Date timestamp, String description, String handler, String origin) {
+    public Alarm(int id, Timestamp timestamp, String description, String handler, String origin) {
         this.id = id;
         this.timestamp = timestamp;
         this.description = description;
@@ -24,5 +24,48 @@ public class Alarm {
 
     public void setId(int id) {
         this.id = id;
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Timestamp timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getHandler() {
+        return handler;
+    }
+
+    public void setHandler(String handler) {
+        this.handler = handler;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    @Override
+    public String toString() {
+        return "Alarm: {" +
+                "id: " + id +
+                ", timestamp: " + timestamp +
+                ", description: " + description +
+                ", handler: " + handler +
+                ", origin: " + origin +
+                '}';
     }
 }
--- AlarmSubscriberJava/src/HTTPAlarmServer.java
+++ AlarmSubscriberJava/src/HTTPAlarmServer.java
@@ -1,5 +1,47 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * Created by Nico Ohler on 28.11.2019.
  */
+
 public class HTTPAlarmServer {
+
+    private ExecutorService executorService = Executors.newCachedThreadPool();
+    DBManager dbManager = new DBManager();
+    // dbManager.connect();
+
+    public HTTPAlarmServer(String directory, int port) {
+        System.out.println("Server started\n" +
+                "Listen for new clients\n-----------------------------------");
+
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            SimpleDateFormat ft = new SimpleDateFormat("dd MM yyyy hh:mm:ss");
+            while (true) {
+                // listening for new clients
+                Socket client = serverSocket.accept();
+
+                // create log
+                Date dNow = new Date();
+                String log = "Client: " + client.getInetAddress() + " connected at " + ft.format(dNow) + "\n";
+                System.out.println(log);
+
+                // start new thread for each client
+                executorService.execute(new ClientHandler(this, client));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        int port = 8080;
+
+        new HTTPAlarmServer("", port);
+    }
 }
