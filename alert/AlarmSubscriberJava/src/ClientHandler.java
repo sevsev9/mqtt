ecc163e6503f03cabed8ba3e08409a261b74f6af
NicoOhler <32089075+NicoOhler@users.noreply.github.com>
2019-12-05 08:42:03 +0100

    Was macht ein Pirat am Computer? Er drückt die Enter-Taste.

--- alert/AlarmSubscriberJava/src/ClientHandler.java
+++ alert/AlarmSubscriberJava/src/ClientHandler.java
@@ -1,3 +1,5 @@
+import com.google.gson.Gson;
+
 import java.io.*;
 import java.net.Socket;
 import java.sql.Timestamp;
@@ -11,24 +13,18 @@ import java.util.Date;
 
 public class ClientHandler implements Runnable {
     private Socket client;
+    private HTTPAlarmServer server;
+    private Gson gson = new Gson();
 
-    private static String BAD_REQUEST = "400 Bad Request";
+   /* private static String BAD_REQUEST = "400 Bad Request";
     private static String OK = "200 OK";
     private static String VERSION = "HTTP/1.1";
-    private final static String LINE = "-----------------------------------";
-
-    private ArrayList<Alarm> alarms;
-    private HTTPAlarmServer server;
+    private final static String LINE = "-----------------------------------";*/
 
     private Timestamp from;
     private Timestamp to;
-
-    private String status = OK;
-    private String filename;
-    private String data = "";
-
-    private String request;
-    private String response;
+    private ArrayList<Alarm> alarms = new ArrayList<>();
+    private String alarmsAsJSON;
 
     public ClientHandler(HTTPAlarmServer server, Socket client) {
         this.client = client;
@@ -43,12 +39,11 @@ public class ClientHandler implements Runnable {
             BufferedWriter out = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
 
             // read and interpret the request
-            request = in.readLine();
-            interpretRequest();
+            interpretRequest(in.readLine());
             //alarms = server.dbManager.getAlarm(from, to);
-            createResponse();
-            System.out.println("\n\tSend response back to client");
-            out.write(response);
+            alarmsAsJSON = gson.toJson(alarms);
+            System.out.println("\n\tCreate response and send it back to client");
+            out.write(createResponse());
             out.flush();
         } catch (IOException e) {
             e.printStackTrace();
@@ -61,55 +56,32 @@ public class ClientHandler implements Runnable {
             }
         }
     }
+    
+    public void interpretRequest(String request) throws IOException {
 
-    /*
-    POST /test HTTP/1.1
-    Host: foo.example
-    Content-Type: text/plain
-    Content-Length: 48
+        String[] lines = request.split("\n");
+        String[] data = lines[5].split("(=|&)");
 
-    From=2019-12-24 23:59:591&To=2020-01-20 00:00:00
-     */
-
-    public void interpretRequest() throws IOException {
-        // todo NullPointerException, request is null, 40s
-        if (request == null) {
-            System.out.println("Empty request");
-            status = BAD_REQUEST;
-        } else {
-            String[] lines = request.split("\n");
+            /*// check if POST
             String[] args = lines[0].split(" ");
-            String[] data = lines[5].split("=|&");
-
-            // check if POST
             if (args[0].toUpperCase().compareTo("POST") != 0)
                 status = BAD_REQUEST;
 
             // check version
             if (args[2].compareTo(VERSION) != 0)
-                status = BAD_REQUEST;
-
-            from = Timestamp.valueOf(data[1]);
-            to = Timestamp.valueOf(data[3]);
-        }
+                status = BAD_REQUEST;*/
+        from = Timestamp.valueOf(data[1]);
+        to = Timestamp.valueOf(data[3]);
     }
 
+    public String createResponse() {
 
-    public void createResponse() {
+        String response = "POST /test HTTP/1.1\n" +
+                "Content-Length: " + alarmsAsJSON.length() + "\n" +
+                "Content-Type: text/html; charset=UTF-8" + "\n\n" + alarmsAsJSON;
 
-        String as = " POST /test HTTP/1.1\n" +
-                "    Host: GeilaHund.com\n" +
-                "    Content-Type: application/x-www-form-urlencoded\n" +
-                "    Content-Length: 48\n" +
-                "\n" +
-                "    From=2019-12-24 23:59:591&To=2020-01-20 00:00:00";
-        System.out.println("\n\tCreate response\n\t" + LINE);
-        String contentLength = "Content-Length: " + data.length() + "\n";
-        String contentType = "Content-Type: text/html; charset=UTF-8" + "\n";
+        System.out.println("Response: " + response);
 
-        response = VERSION + " " + status + "\n" + server + contentLength + contentType + "\n" + data;
-        System.out.println("\t" + VERSION + " " + status + "\n\t" + "\t" + contentLength + "\t" + contentType + "\n\t" + data);
-
-        response = "POST "
+        return response;
     }
 }
