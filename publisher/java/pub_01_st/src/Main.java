5bb29dfd90e522b3dc40f30b10294ef3874196e2
Simon <stockinger.sim@gmail.com>
2019-12-05 08:56:36 +0100

    pub commit

--- publisher/java/pub_01_st/src/Main.java
+++ publisher/java/pub_01_st/src/Main.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+import org.json.simple.*;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Main {
+    public static void main(String[] args) {
+        JSONObject sampleObject = new JSONObject();
+        sampleObject.put("name", "Stackabuser");
+        sampleObject.put("age", 35);
+
+        JSONArray messages = new JSONArray();
+        messages.add("Hey!");
+        messages.add("What's up?!");
+
+        sampleObject.put("messages", messages);
+        try(FileWriter fw = new FileWriter("testfile.txt")){
+            fw.write(sampleObject.toJSONString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
