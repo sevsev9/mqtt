e31cc391dc46900b99c7b726d00c9ce406cfc565
MarcKoppler <32321252+MarcKoppler@users.noreply.github.com>
2019-12-05 08:18:19 +0100

    mongodb js updated

--- NodeJS/Node-mongodb/index.js
+++ NodeJS/Node-mongodb/index.js
@@ -1,2 +1,8 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://root:toor@localhost:27017/test', {useNewUrlParser: true});
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    // we're connected!
+});
