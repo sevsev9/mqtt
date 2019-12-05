9e32f1051560ec50d99fc9bcfadb44bbe8373630
JohannesKoettstorfer <JohannesKoettstorfer@users.noreply.github.com>
2019-12-05 10:07:45 +0100

    Update sandbox

--- frontend/component_sandboxes/koettstorfer/koettstorfer_sandbox/src/App.vue
+++ frontend/component_sandboxes/koettstorfer/koettstorfer_sandbox/src/App.vue
@@ -1,17 +1,53 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Graph :oildata="oildata"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Graph from "./components/Graph";
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Graph
+  }, data() {
+    return {
+      oildata: [
+        {
+          "I": 1,
+          "ti": "2019-04-20",
+          "te": 10.23,
+          "p": 5.49
+        }, {
+          "I": 1,
+          "ti": "2019-04-21",
+          "te": 10.5,
+          "p": 5.6
+        }, {
+          "I": 1,
+          "ti": "2019-04-22",
+          "te": 10.9,
+          "p": 5.8
+        }, {
+          "I": 1,
+          "ti": "2019-04-23",
+          "te": 11.3,
+          "p": 6.0
+        }, {
+          "I": 1,
+          "ti": "2019-04-24",
+          "te": 11.7,
+          "p": 6.2
+        }, {
+          "I": 1,
+          "ti": "2019-04-25",
+          "te": 10.23,
+          "p": 5.49
+        }
+      ]
+    }
   }
 }
 </script>
--- frontend/component_sandboxes/koettstorfer/koettstorfer_sandbox/src/components/SampleProvider.vue
+++ frontend/component_sandboxes/koettstorfer/koettstorfer_sandbox/src/components/SampleProvider.vue
@@ -1,5 +1,5 @@
 <template>
-    
+    <h1>I am a </h1>
 </template>
 
 <script>
