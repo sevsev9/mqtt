76b12b6b0442f378b2e9235f5dbbb2b4d7ae2632
MarcKoppler <32321252+MarcKoppler@users.noreply.github.com>
2019-12-12 08:30:51 +0100

    NodeJS Mongoose ReadWrite added

--- api/node_database/index.js
+++ api/node_database/index.js
@@ -4,28 +4,36 @@ mongoose.connect('mongodb://ahit5:niceDayToday@docker.htl-wels.at:27017/5ahit', 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
-    var Sensordata = mongoose.Schema({
-        time: Object,
-        temperature: Number,
-        humidity: Number,
-        pressure: Number
+    //Connected
+});
+var Sensordata = mongoose.Schema({
+    time: Object,
+    temperature: Number,
+    humidity: Number,
+    pressure: Number
+});
+
+var Sensor = mongoose.model('Sensor', Sensordata, 'Sensordata')
+
+function Write(time, temperature, humidity, pressure) {
+
+    var Sens = new Sensor({
+        time: time.split('.'),
+        temperature: temperature,
+        humidity: humidity,
+        pressure: pressure
     });
 
-    var Sensor = mongoose.model('Sensor', Sensordata, 'Sensordata')
+    Sens.save(function (err) {
+        if (err) return console.error(err);
+    })
+}
 
-    function Write(time, temperature, humidity, pressure) {
-
-        var Sens = new Sensor({
-            time: time.split('.'),
-            temperature: temperature,
-            humidity: humidity,
-            pressure: pressure
-        });
-
-        Sens.save(function (err) {
-            if (err) return console.error(err);
-        })
-    }
-});
+function Read(){
+    var res;
+    Sensor.find().lean().exec(function (err, Sensordata) {
+        return res.end(JSON.stringify(Sensordata));
+    })
+}
 
 
